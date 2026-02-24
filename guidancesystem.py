8c16040163867aebc48d01adaa0afbcf5cd651a9
Bmcneley <154935809+Bmcneley@users.noreply.github.com>
2026-02-23 21:47:41 -0600

    Procedure

--- guidancesystem.py
+++ guidancesystem.py
@@ -1,7 +1,19 @@
 import numpy as np
 import matplotlib as plt
+from matplotlib.animation import FuncAnimation
+
 #Missile Guidance System Simulation
 #Brice McNeley, Fernando Sanchez
+#Numpy usage: np.(cmd) ; matplotlib usage: plt.(cmd)
+#Procedure:
+#1: Specify engagement conditions / parameters for the persuer and the target
+#2: Determine initial conditions for kinematics
+#3: Compute ProNav (Proportional Navigation) input at each time step
+#4: Compute ProNav command and advance kinematics forward one time step (Very small dt)
+#5: Plot the kinematics of the two bodies in real time w/ matplotlib
+
+
+
 
 
 
